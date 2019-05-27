9a9944e19f8ac3ef04f2a2c1621187437c976cf8
ankoks <koksharov.work@gmail.com>
2019-05-27 16:34:50 +0300

    1. Add vuetify plugin 2. Add different directives

---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul>
+        <li v-for="(todo, index) in todos"
+            :key="index"
+            v-on:click="deleteTodo(index)">{{ todo }}</li>
+    </ul>
+
+</template>
+
+<script>
+    export default {
+        props: ['todos'],
+        methods: {
+            deleteTodo(index) {
+                this.$emit('delete-todo', index)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
