fccf8f0c38eb108d8bc6a3e9c4554b43834bc500
ankoks <koksharov.work@gmail.com>
2019-06-05 11:22:19 +0300

    Add axios library

--- src/views/ActionsPage.vue
+++ src/views/ActionsPage.vue
@@ -1,14 +1,23 @@
 <template>
-  <h1>Welcome {{ usr }} <!--({{ usr2 }})--> to Axios demonstrating page</h1>
+  <v-app>
+    <h1>Welcome {{ usr }} <!--({{ usr2 }})--> to Axios demonstrating page</h1>
+    <div v-for="(currency, index) in info" :key="index" class="currency">{{ currency.description }}:
+      <span class="lighten">
+      <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
+    </span>
+    </div>
+  </v-app>
 </template>
 
 <script>
   // import {eventEmitter} from '../main'
+  import axios from 'axios';
 
   export default {
     name: "ActionsPage",
     data() {
       return {
+        info: null
         // usr2: this.$router.currentRoute.query['name']
       }
     },
@@ -16,7 +25,18 @@
       usr() {
         return this.$router.currentRoute.params['name']
       }
+    },
+    mounted() {
+      axios
+          .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+          .then(response => (this.info = response.data.bpi));
+    },
+    filters: {
+      currencydecimal(value) {
+        return value.toFixed(2)
+      }
     }
+
   }
 </script>
 
