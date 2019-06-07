4ffc141bf3cefdddf510c136ecaed6fa0fcfe82b
ankoks <koksharov.work@gmail.com>
2019-06-07 17:25:31 +0300

    Add profile content

--- src/components/views/Profile.vue
+++ src/components/views/Profile.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="div__black">
     <h1>Profile</h1>
-    <v-card class="mx-auto hide-overflow" style="max-width: 600px;">
+    <v-card class="mx-auto" style="max-width: 500px;">
       <v-layout>
-        <v-flex xs6 d-flex>
+        <v-flex xs6 d-flex class="scale">
           <v-img src="https://cdn.vuetifyjs.com/images/ratings/fortnite1.png"></v-img>
         </v-flex>
         <v-flex xs6>
           <v-container grid-list-md pa-0 pl-2 style="margin: -4px 0">
             <v-layout wrap>
-              <v-flex xs7 d-flex>
+              <v-flex xs7 d-flex class="scale">
                 <v-img src="https://cdn.vuetifyjs.com/images/ratings/fortnite2.png"></v-img>
               </v-flex>
-              <v-flex xs5 d-flex>
+              <v-flex xs5 d-flex class="scale">
                 <v-img src="https://cdn.vuetifyjs.com/images/ratings/fortnite3.png"></v-img>
               </v-flex>
-              <v-flex xs5 d-flex>
+              <v-flex xs5 d-flex class="scale">
                 <v-img src="https://cdn.vuetifyjs.com/images/ratings/fortnite4.png"></v-img>
               </v-flex>
-              <v-flex xs7 d-flex>
+              <v-flex xs7 d-flex class="scale">
                 <v-img src="https://cdn.vuetifyjs.com/images/ratings/fortnite5.png"></v-img>
               </v-flex>
             </v-layout>
@@ -36,7 +36,7 @@
       <v-card-actions>
         <span class="pl-2 grey--text text--darken-2 font-weight-light caption">16,544 reviews</span>
         <v-spacer></v-spacer>
-        <v-rating length="10" hover>
+        <v-rating length="10" hover dense>
         </v-rating>
       </v-card-actions>
       <div class="pa-3 pt-0 caption">
@@ -56,5 +56,13 @@
 <style scoped>
   .div__black {
     color: white;
+  }
+  .scale {
+    z-index: 1;
+    transition: 0.2s; /* Время эффекта */
+  }
+  .scale:hover {
+    z-index: 2;
+    transform: scale(1.5); /* Увеличиваем масштаб */
   }
 </style>
